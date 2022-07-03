b6dc10f69fb1e15fb9bb3c908aeb895a59bc04d5
tigawanna <denniskinuthiaw@gmail.com>
2022-07-03 09:49:49 +0300

    added bg color to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { TheTable } from './TheTable/TheTable';
 
 function App() {
   return (
-    <div className="w-screen h-screen">
+    <div className="w-screen h-screen bg-slate-700">
      <TheTable/>
     </div>
   );
