5457784a0fe4e348460c0e4bee65a93856c355f0
tigawanna <denniskinuthiaw@gmail.com>
2022-07-03 09:47:41 +0300

    init files

---
+++ src/TheTable/TheTable.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface TheTableProps {
+
+}
+
+export const TheTable: React.FC<TheTableProps> = ({}) => {
+return (
+ <div className='w-full h-full'>
+
+ </div>
+);
+}
